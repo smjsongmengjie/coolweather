57b18dca2c686cee82c6d44eebdc1e50c116a050
Smj <smj@email.com>
2018-09-01 12:06:35 +0800

    加入显示天气信息的功能

---
+++ app/src/main/java/com/coolweather/android/gson/Forecast.java
@@ -0,0 +1,35 @@
+package com.coolweather.android.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * @author Admin
+ * @version $Rev$
+ * @des ${TODO}
+ * @updateAuthor $Author$
+ * @updateDes ${TODO}
+ */
+public class Forecast {
+
+    public String date;
+
+    @SerializedName("tmp")
+    public Temperature temperature;
+
+    @SerializedName("cond")
+    public More more;
+
+    public class Temperature{
+
+        public String max;
+
+        public String min;
+
+    }
+
+    public class More{
+        @SerializedName("txt_d")
+        public String info;
+    }
+
+}
